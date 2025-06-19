986a8ceabb8a7b5100052665ce97cc42b4a036ac
Danylo Perepeliuk <danyla958@gmail.com>
2025-06-19 17:00:46 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+rootProject.name = "Ktorin"
+
+pluginManagement {
+    repositories {
+        google {
+            content { 
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            content { 
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        mavenCentral()
+    }
+}
+include(":ktor-inspektor")
+include(":sample:composeApp")
+
