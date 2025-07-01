9e1432cb89508e7c48c408177d45e277bf67fabf
Danylo Perepeliuk <danyla958@gmail.com>
2025-07-01 03:40:20 +0300

    - Update kotlinx-datetime version and refactor time formatting logic

--- axer/src/commonMain/kotlin/io/github/orioneee/extentions/LongExt.kt
+++ axer/src/commonMain/kotlin/io/github/orioneee/extentions/LongExt.kt
@@ -1,20 +1,19 @@
 package io.github.orioneee.extentions
 
-import kotlinx.datetime.Instant.Companion.fromEpochMilliseconds
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.format
 import kotlinx.datetime.format.FormatStringsInDatetimeFormats
 import kotlinx.datetime.format.byUnicodePattern
 import kotlinx.datetime.toLocalDateTime
-import kotlinx.datetime.toStdlibInstant
 import kotlin.time.ExperimentalTime
+import kotlin.time.Instant.Companion.fromEpochMilliseconds
 
 @OptIn(FormatStringsInDatetimeFormats::class, ExperimentalTime::class)
 internal fun Long.formateAsTime(): String {
     val instant = fromEpochMilliseconds(this)
     val timeZone = TimeZone.currentSystemDefault()
-    val dateTime = instant.toStdlibInstant().toLocalDateTime(timeZone)
+    val dateTime = instant.toLocalDateTime(timeZone)
 
     val format = LocalDateTime.Format {
         byUnicodePattern("HH:mm:ss")
