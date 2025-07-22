eed6a84b431141ab80c992fbb9521bc154d27cea
Danylo Perepeliuk <danyla958@gmail.com>
2025-07-23 01:00:50 +0300

    updated proguard-rules.pro and enabled minify

--- axer-remote-debugger/build.gradle.kts
+++ axer-remote-debugger/build.gradle.kts
@@ -96,6 +96,10 @@ android {
         debug {
             isDebuggable = false
             isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
         release {
             isMinifyEnabled = true
