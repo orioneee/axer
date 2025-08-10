4677469c937877e5b6ca9427ac6d2677870c2f2e
Danylo Perepeliuk <danyla958@gmail.com>
2025-08-10 14:04:56 +0300

    Package refactoring

--- axer-remote-debugger/src/androidMain/kotlin/io/github/orioneee/MainActivity.kt
+++ axer-remote-debugger/src/androidMain/kotlin/io/github/orioneee/MainActivity.kt
@@ -8,8 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import io.github.orioneee.internal.presentation.components.AxerTheme
 import io.github.orioneee.navigation.NavigationClass
-import io.github.orioneee.presentation.components.AxerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
